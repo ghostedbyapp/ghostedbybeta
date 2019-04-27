d3d57ac05df8c20d612dadd7068905e6fdb1813a
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-04-27 13:24:08 -0400

    made build

---
+++ client/build/precache-manifest.face817b6629c7263ba570e218080caa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9af55dbcacf487fde127",
+    "url": "/static/js/main.2fa18554.chunk.js"
+  },
+  {
+    "revision": "ee3eb3340190af686653",
+    "url": "/static/js/2.535f98f5.chunk.js"
+  },
+  {
+    "revision": "9af55dbcacf487fde127",
+    "url": "/static/css/main.b9599bf9.chunk.css"
+  },
+  {
+    "revision": "5bdfdc1631bcf56e10977af3ebe0632c",
+    "url": "/index.html"
+  }
+];
