72f8bc7d6860ba0344bb65bfedfdc13d2cb3d6da
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-04-27 13:30:08 -0400

    removed navbar toggle button on smaller screen sizes

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -15,12 +15,6 @@ function NavBar() {
                 <a className="navbar-brand mr-auto" href="/">
                     <strong style={styles}>GhostedBy</strong>
                 </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-distance="-250">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="d-none d-md-block text-uppercase">
                     <ul className="navbar-nav"></ul>
                 </div>
