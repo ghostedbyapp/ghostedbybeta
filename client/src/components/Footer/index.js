b20aade9afa4db0fb833d2d26299f355d539b865
LeRon Jackson <leronj23@gmail.com>
2019-05-02 19:40:39 -0400

    update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom"
 
+var styles = {
+  color: '#000000'
+};
+
 function Footer() {
     return (
       // footer area
@@ -24,7 +28,6 @@ function Footer() {
                 <li className="mb-2">
                   <h6 className="text-uppercase">Terms & Conditions</h6>
                 </li>
-                {/* <li className="text-muted"><a href="/privacy">Privacy</a></li> */}
                 <Link to="/privacy">Privacy</Link>
               </ul>
             </div>
@@ -32,7 +35,7 @@ function Footer() {
               <p>Please submit companies you've been GhostedBy to this database with integrity and honesty as this is
                 intended to be a source for candidates to share and research company activities for a more productive job
                 search. GhostedBy is a crowdsource database collecting the wisdom of the crowd to report companies who ghost
-                candidates. Please report responsibly.</p>
+                candidates. Please report responsibly.</p><Link to="/admin" style={styles}>Privacy</Link>
             </div>
           </div>
         </div>
